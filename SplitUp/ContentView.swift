4c0d8b00a50f80a7e1853fe25612ab3cc990e55d
Yash Poojary <yashspoojary245@gmail.com>
2021-09-15 13:23:14 +0530

    Added layout code and app images

--- SplitUp/ContentView.swift
+++ SplitUp/ContentView.swift
@@ -8,9 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipAmount = Double(tipPercentages[tipPercentage])
+        let checkTotal = Double(checkAmount) ?? 0
+        
+        let tipFigure = checkTotal * tipAmount / 100
+        let finalTotal = checkTotal + tipFigure
+        let perPerson = finalTotal / peopleCount
+
+        return perPerson
+    }
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    Picker("Number Of People", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                
+                Section(header: Text("How much would you like to tip")) {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(0..<tipPercentages.count) {
+                            Text("\(tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+            
+                
+                
+                Section {
+                    Text("₹\(totalPerPerson, specifier: "%.2f")")
+                }
+            }
+            .navigationBarTitle("SplitUp")
+        }
     }
 }
 
