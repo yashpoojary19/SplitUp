917100e7d83ed81d6b06de0e6e31cda56d26ab50
Yash Poojary <yashspoojary245@gmail.com>
2021-09-15 11:53:29 +0530

    Initial Commit

---
+++ SplitUp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SplitUp
+//
+//  Created by Yash Poojary on 15/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SplitUp/SplitUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitUpApp.swift
+//  SplitUp
+//
+//  Created by Yash Poojary on 15/09/21.
+//
+
+import SwiftUI
+
+@main
+struct SplitUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
